14271046ebb3fa6bb0223ff168c7b68db7d94e39
Jonathan Catanio <JonCatanio@Hoth.local>
2015-11-21 17:36:29 -0800

    Added coffeescript syntax detect vim file. Updated setup script to include added files.

--- scripts/CreateSymLinks.sh
+++ scripts/CreateSymLinks.sh
@@ -5,6 +5,10 @@ ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 ln -s ~/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
 
+# Set up CoffeeScript syntax highlighting for vim.
+ln -s ~/dotfiles/vim/syntax/coffee.vim ~/.vim/syntax/coffee.vim
+ln -s ~/dotfiles/vim/ftdetect/coffee.vim ~/.vim/ftdetect/coffee.vim
+
 # Set up bash links.
 ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
